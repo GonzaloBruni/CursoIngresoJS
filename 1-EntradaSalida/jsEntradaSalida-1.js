2a21ae55f8510f749d111969120f1f4372204d1e
GonzaloBruni <60743479+GonzaloBruni@users.noreply.github.com>
2020-02-06 11:12:15 -0300

    Ejercicio primero de la Clase 1

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,53 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	// Es un valor literal si esta entre ""
+	//alert("Esto funciona de maravilla"); // Doble barra para agregar comentario, al lado tambien se puede al lado
+	// comentar todo un parrafo entre Barra y Asterisco
+	/*Enunciado:
+    Bienvenidos.
+    Al presionar el botón, se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla".*/
+    //Enunciado Siempre Arriba de todo, apenas lo den y se cierra
+   //"alert" es una funcion de un texto que lo lee y lo muestra, puede ser un texto literal o no
+   //Las aplicaciones manejan la memoria RAM como quieren, Chrome la usa utilizando el comando "VAR"
+   //Las variable se escriben una sola vez ya que con TAB se auto-escribe solo.
+   //var apellido;
+   //appellido="perez";
+   //var apellido;
+   //apellido="perez";
+   //var PrimerPrecio;
+   //var SegundoPrecio; 
+   //A la variable nunca se le puede llamar Respuesta, Retorno
+   //Detallar cada Variable, con nombre nunca con Numero
+   //Se puede hacer pero intentar que no, es Definir dos variables al mismo tiempo. Siempre las Variables van arriba de todo y con su propio renglon
+   //siempre ante un resultado poner cada variable
+   
+    var PrimerPrecio;
+    var SegundoPrecio;
+    var apellido;
+    apellido="perez";
+    //es un valor literal si esta entre""
+    alert("tal cual");//literal
+    var primerPrecio;
+	var	segundoPrecio;
+	var apellido;
+	apellido="perez";//Al estar apellido igual a perez, sin las comillas aparece igual la alerta mostrando PEREZ porque es la Equivalencia que uno le Dio
+	//alert("tal cual");//literal
+	alert(apellido); //literal
+	alert("apellido"); //variable
+	alert(); //sale vacio
+	alert(primerPrecio); //devuelve una palabra "Undefined"
+	alert(preciopri); //f12 para mostrar si hubo algun error no me sale error, pero si sale null no se puede hacer nada
+
+	var apellido="nadie";
+	//asignacion: por orde de arriba hacia abajo
+	var apellido="perez";
+	alert(apellido);
+	//Para el Proceso de hacer el BreakPoint  Elegir el punto de "accion" y despues accionar la pagina, presionar F11 para ir paso a paso en cada accion que se esta ejecutando
+
+
+
+
 }
+
 
